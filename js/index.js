9b1e4ceb570c7ea1a06d3bd948c180f94d14fa2f
schrodog <sky_sun_tg@hotmail.com>
2018-03-30 14:27:26 +0800

    beautify

--- js/index.js
+++ js/index.js
@@ -1,104 +1,96 @@
-function sign_up(){
+'use strict'
+
+function sign_up() {
   var inputs = document.querySelectorAll('.input_form_sign');
-document.querySelectorAll('.ul_tabs > li')[0].className=""; 
-document.querySelectorAll('.ul_tabs > li')[1].className="active"; 
-  
-  for(var i = 0; i < inputs.length ; i++  ) {
-if(i == 2  ){
+  document.querySelectorAll('.ul_tabs > li')[0].className = "";
+  document.querySelectorAll('.ul_tabs > li')[1].className = "active";
 
-}else{  
-document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
-}
-} 
+  for (var i = 0; i < inputs.length; i++) {
+    if (i == 2) {
 
-setTimeout( function(){
-for(var d = 0; d < inputs.length ; d++  ) {
- document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block active_inp";  
-   }
+    } else {
+      document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
+    }
+  }
 
+  setTimeout(function() {
+    for (var d = 0; d < inputs.length; d++) {
+      document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block active_inp";
+    }
+  }, 100);
 
- },100 );
- document.querySelector('.link_forgot_pass').style.opacity = "0";
-   document.querySelector('.link_forgot_pass').style.top = "-5px";
-   document.querySelector('.btn_sign').innerHTML = "SIGN UP";    
-  
-  setTimeout(function(){
+  document.querySelector('.link_forgot_pass').style.opacity = "0";
+  document.querySelector('.link_forgot_pass').style.top = "-5px";
+  document.querySelector('.btn_sign').innerHTML = "SIGN UP";
+
+  setTimeout(function() {
     document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_none";
-
-  },450);
+  }, 450);
 
 }
 
 
 
-function sign_in(){
+function sign_in() {
   var inputs = document.querySelectorAll('.input_form_sign');
-document.querySelectorAll('.ul_tabs > li')[0].className = "active"; 
-document.querySelectorAll('.ul_tabs > li')[1].className = ""; 
-  
-  for(var i = 0; i < inputs.length ; i++  ) {
-switch(i) {
-    case 1:
- console.log(inputs[i].name);
+  document.querySelectorAll('.ul_tabs > li')[0].className = "active";
+  document.querySelectorAll('.ul_tabs > li')[1].className = "";
+
+  for (var i = 0; i < inputs.length; i++) {
+    switch (i) {
+      case 1:
+        console.log(inputs[i].name);
         break;
-    case 2:
- console.log(inputs[i].name);
-    default: 
-document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
-}
-} 
-
-setTimeout( function(){
-for(var d = 0; d < inputs.length ; d++  ) {
-switch(d) {
-    case 1:
- console.log(inputs[d].name);
-        break;
-    case 2:
- console.log(inputs[d].name);
-
-    default:
- document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block";  
- document.querySelectorAll('.input_form_sign')[2].className = "input_form_sign d_block active_inp";  
-
-   }
+      case 2:
+        console.log(inputs[i].name);
+      default:
+        document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
+    }
   }
- },100 );
 
+  setTimeout(function() {
+    for (var d = 0; d < inputs.length; d++) {
+      switch (d) {
+        case 1:
+          console.log(inputs[d].name);
+          break;
+        case 2:
+          console.log(inputs[d].name);
 
+        default:
+          document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block";
+          document.querySelectorAll('.input_form_sign')[2].className = "input_form_sign d_block active_inp";
+      }
+    }
+  }, 100);
 
-  setTimeout(function(){
+  setTimeout(function() {
+    document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_block";
+  }, 500);
 
-document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_block";
+  setTimeout(function() {
+    document.querySelector('.link_forgot_pass').style.opacity = "1";
+    document.querySelector('.link_forgot_pass').style.top = "5px";
 
- },500);
+    for (var d = 0; d < inputs.length; d++) {
+      switch (d) {
+        case 1:
+          console.log(inputs[d].name);
+          break;
+        case 2:
+          console.log(inputs[d].name);
 
-  setTimeout(function(){
-
- document.querySelector('.link_forgot_pass').style.opacity = "1";
-   document.querySelector('.link_forgot_pass').style.top = "5px";
-    
-
-for(var d = 0; d < inputs.length ; d++  ) {
-
-switch(d) {
-    case 1:
- console.log(inputs[d].name);
-        break;
-    case 2:
- console.log(inputs[d].name);
-
-         break;
-    default:
- document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign";  
+          break;
+        default:
+          document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign";
+      }
+    }
+  }, 1500);
+  document.querySelector('.btn_sign').innerHTML = "SIGN IN";
 }
-  }
-   },1500);
-   document.querySelector('.btn_sign').innerHTML = "SIGN IN";    
-}
 
-
-window.onload =function(){
+window.onload = function() {
   document.querySelector('.cont_middle').className = "cont_middle cent_active";
-
 }
+
+
